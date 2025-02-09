8b089c9dc89ef4b4ec324e2e1da469e0abe7e717
Galesy <heitor@estevam.org>
2025-02-09 10:01:57 -0300

    added lights

--- scenes/game/player/player.gd
+++ scenes/game/player/player.gd
@@ -2,11 +2,22 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+var HP: float = 20.0
+const MAX_HP: float = 20.0
+
+@onready var hitbox = $Hitbox
+@onready var anims = $AnimationPlayer
+
+func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		velocity = direction * SPEED
+		hitbox.rotation = direction.angle()
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, 0.6)
 	
 	move_and_slide()
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("ui_accept"):
+		anims.play("attack")
